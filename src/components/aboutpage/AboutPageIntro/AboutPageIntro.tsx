3d306680a3aad61211d19155383473afbbbdf601
Chris Ware <c.p.ware@hotmail.com>
2025-05-19 10:55:41 -0700

    About page intro

--- src/components/aboutpage/AboutPageIntro/AboutPageIntro.tsx
+++ src/components/aboutpage/AboutPageIntro/AboutPageIntro.tsx
@@ -1,5 +1,5 @@
-import LayoutWrapper from "@/components/shared/LayoutWrapper";
 import styles from "./AboutPageIntro.module.css";
+import LayoutWrapper from "@/components/shared/LayoutWrapper";
 import Nav from "@/components/shared/Nav/Nav";
 import SectionHeading from "@/components/shared/SectionHeading/SectionHeading";
 import Image from "next/image";
@@ -51,15 +51,38 @@ export default function AboutPageIntro() {
             </div>
           </div>
           <div className={styles.textContainer}>
-            <p className={styles.text}>
-              At Nier Transportation, we&lsquo;re more than a car service; we&lsquo;re your
-              trusted partner in high end transportation. With a passion for
-              providing the best car service in the Phoenix Metro area, we&lsquo;ve
-              been serving the Arizona community for 20 years. Our mission is
-              clear: to provide an unparalleled, high end, late model car
-              service near you while ensuring your comfort, safety, and peace of
-              mind.
+            <p className={styles.textHeading}>
+              At Nier Transportation, we&lsquo;re more than a car service;
+              we&lsquo;re your trusted partner in high end transportation. With
+              a passion for providing the best car service in the Phoenix Metro
+              area, we&lsquo;ve been serving the Arizona community for 20 years.
+              Our mission is clear: to provide an unparalleled, high end, late
+              model car service near you while ensuring your comfort, safety,
+              and peace of mind.
             </p>
+            <div className={styles.textContainerBottom}>
+              <div className={styles.textContainerBottomLeft}>
+                <span className={styles.sectionHeading}>About us</span>
+                <p className={styles.text}>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Impedit quaerat quasi fuga laborum doloremque eligendi, soluta
+                  minima nam sequi perferendis cum voluptates quos officiis
+                  inventore ducimus iusto fugiat similique, ad sit, non tempore
+                  dolores laboriosam. Suscipit assumenda, unde accusantium
+                  officia soluta aut fugiat magnam architecto libero nesciunt
+                  inventore non illo.
+                </p>
+              </div>
+              <p className={styles.text}>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Impedit quaerat quasi fuga laborum doloremque eligendi, soluta
+                minima nam sequi perferendis cum voluptates quos officiis
+                inventore ducimus iusto fugiat similique, ad sit, non tempore
+                dolores laboriosam. Suscipit assumenda, unde accusantium officia
+                soluta aut fugiat magnam architecto libero nesciunt inventore
+                non illo.
+              </p>
+            </div>
           </div>
         </div>
       </LayoutWrapper>
