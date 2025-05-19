849e6000865dc75292a74746750744c3e66f8810
Chris Ware <c.p.ware@hotmail.com>
2025-05-19 10:28:03 -0700

    Final cta updated

--- src/components/shared/FinalCTA/FinalCTA.tsx
+++ src/components/shared/FinalCTA/FinalCTA.tsx
@@ -1,5 +1,4 @@
 import BookingButton from "@/components/homepage/BookingButton/BookingButton";
-import LayoutWrapper from "../LayoutWrapper";
 import styles from "./FinalCTA.module.css";
 import SectionHeading from "../SectionHeading/SectionHeading";
 
@@ -7,14 +6,12 @@ export default function FinalCTA() {
   return (
     <section className={styles.container}>
       <SectionHeading title='book now' />
-      <LayoutWrapper>
-        <div className={styles.content}>
-          <BookingButton text='Book Here • Book Here • Book Here • Book Here •' />{" "}
-          <h2 className={styles.heading}>
-            Have a question or just want to say hello? We&lsquo;re here for you!
-          </h2>
-        </div>
-      </LayoutWrapper>
+      <div className={styles.content}>
+        <BookingButton text='Book Here • Book Here • Book Here • Book Here •' />{" "}
+        <h2 className={styles.heading}>
+          Have a question or just want to say hello? We&lsquo;re here for you!
+        </h2>
+      </div>
     </section>
   );
 }
