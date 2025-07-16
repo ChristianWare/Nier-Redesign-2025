fe3a5790bde9273b1f634d070bd36c46ed4596a3
Chris Ware <c.p.ware@hotmail.com>
2025-07-16 00:23:37 -0700

    Auth done

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,6 +2,7 @@ import { auth } from "../../../auth";
 
 export default async function DashboardPage() {
   const session = await auth();
+  console.log(session);
   return (
     <div>
       <h1>{session?.user.name}'s Dashboard</h1>
